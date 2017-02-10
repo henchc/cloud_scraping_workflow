9e9a1c5b1240c7cd67c242882230503dfac66f68
Chris Hench <chris.l.hench@gmail.com>
2017-02-09 16:33:20 -0800

    init commit

---
+++ push_to_box.md
@@ -0,0 +1,34 @@
+# Push scraped files from remote server to Box account
+(Adapted from [BRC directions](https://github.com/ucberkeley/brc-draft-documentation/wiki/Transferring-Data-Between-Savio-and-Your-UC-Berkeley-Box-Account) to transfer from Savio)
+
+First zip together all the files you want to push to Box, you can't (or don't want to) `put` a file structure.
+
+```
+zip -r name_of_zip.zip scraped_files/
+```
+
+If your zip file will exceeds the allotted maximum file size, you can split your zip file. For 1GB files:
+
+```
+zip -r -s 1000m name_of_zip.zip scraped_files/
+```
+
+Now we use [LFTP](http://lftp.tech) to push the zip file to Box:
+
+```
+lftp ftp.box.com
+```
+
+For encryption:
+
+```
+set ssl-allow true
+```
+
+Then enter you username and password.
+
+You can now use basic bash commands to navigate the file structure of your Box account. Use `pwd`, `cd`, `ls`, and `mkdir` to create a new directory and navigate into it. Then to transfer the file into the working directory of your Box account we use `put`: 
+
+```
+put name_of_zip.zip
+```
---
+++ scraping_setup.sh
@@ -0,0 +1,46 @@
+# download miniconda and install
+
+wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+sleep 10
+
+chmod +x Miniconda-latest-Linux-x86_64.sh
+bash Miniconda-latest-Linux-x86_64.sh
+
+sleep 10
+
+# install relevant scraping packages
+
+pip install bs4
+pip install lxml
+pip install selenium
+
+
+# download chrome
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sleep 10
+
+sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
+
+# download chrome driver
+# help from https://christopher.su/2015/selenium-chromedriver-ubuntu/
+# and http://blog.likewise.org/2015/01/setting-up-chromedriver-and-the-selenium-webdriver-python-bindings-on-ubuntu-14-dot-04/
+
+LATEST=$(wget -q -O - http://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget http://chromedriver.storage.googleapis.com/$LATEST/chromedriver_linux64.zip
+
+sleep 10
+
+# unzip chromedriver and put in share and binaries
+
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
+sudo mv -f chromedriver /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
+
+# install LFTP for transfer of scraped files to Box
+
+sudo apt-get install lftp
